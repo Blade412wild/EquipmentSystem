775c175cab6cedecab6af3b619c521c119a1109a
Blade412wild <nathanhoh951@gmail.com>
2024-12-10 20:19:11 +0100

    Added models

---
+++ Assets/01_Scripts/IGrabAble.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGrabAble
+{
+    public Transform holdPos { get; set; } 
+
+
+}
---
+++ Assets/01_Scripts/Rock.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour, IGrabAble
+{
+    public Transform holdPos { get; set; }
+
+}
